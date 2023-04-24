4c3af00c2c72febab28516d01d949bc13c4f77ad
prashantkumar06 <prashant.kumar06@nagarro.com>
2023-04-24 10:34:44 +0530

    Hello

--- Travel Management System/src/travel/management/system/About.java
+++ Travel Management System/src/travel/management/system/About.java
@@ -33,27 +33,6 @@ public class About extends JFrame implements ActionListener {
         Font f = new Font("RALEWAY", Font.BOLD, 180);
         setFont(f);
 
-        s = "                                    About Projects          \n  "
-                + "\nThe objective of the Travel and Tourism Management System"
-                + "project is to develop a system that automates the processes "
-                + "and activities of a travel and the purpose is to design a "
-                + "system using which one can perform all operations related to "
-                + "traveling.\n\n"
-                + "This application will help in accessing the information related "
-                + "to the travel to the particular destination with great ease. "
-                + "The users can track the information related to their tours with "
-                + "great ease through this application. The travel agency information "
-                + "can also be obtained through this application.\n\n"
-                + "Advantages of Project:"
-                + "\nGives accurate information"
-                + "\nSimplifies the manual work"
-                + "\nIt minimizes the documentation related work"
-                + "\nProvides up to date information"
-                + "\nFriendly Environment by providing warning messages."
-                + "\ntravelers details can be provided"
-                + "\nbooking confirmation notification"
-                ;
-
         TextArea t1 = new TextArea(s, 10, 40, Scrollbar.VERTICAL);
         t1.setEditable(false);
         t1.setBounds(20, 100, 450, 300);
